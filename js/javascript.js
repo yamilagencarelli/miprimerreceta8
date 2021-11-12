2d8f536b01fe93c5bcd577cfb83484e8957f7622
yamilagencarelli <89711211+yamilagencarelli@users.noreply.github.com>
2021-11-12 01:17:26 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+function cartel() {
+  alert ("Pagina creada por Gencarelli Yamila, para el curso de Programación Web - Potrero Digital.")
+}
+
+function cambio() {
+  document.body.style.backgroundColor="black";
+  document.body.style.color="orange";
+  document.body.style.fontSize="x-large";
+  }
